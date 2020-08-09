32d8b3b3a55a6a7288190607fcea361d6f88e7f5
U-Zo <kwoojo0127@naver.com>
2020-08-09 20:48:48 +0900

    Add button component to apply actions

---
+++ front-end/src/components/common/Button.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+
+const StyledButton = styled.button`
+  border: none;
+  border-radius: 4px;
+  font-weight: bold;
+  padding: 0.5rem 1rem;
+  color: #fff;
+  outline: none;
+  cursor: pointer;
+  font-size: 1rem;
+  background-color: #3D79F2;
+  
+  &:hover {
+    background-color: #5e8de9;
+  }
+  
+  ${props =>
+    props.fullWidth &&
+    css`
+      width: 100%;
+    `}
+`;
+
+const Button = (props) => {
+    return <StyledButton {...props}/>
+}
+
+export default Button;
