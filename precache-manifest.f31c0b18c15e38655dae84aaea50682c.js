8510f612fc1c9fb458a383e914a6958172563e6c
U-Zo <kwoojo0127@naver.com>
2020-09-06 00:27:51 +0900

    Updates

---
+++ precache-manifest.f31c0b18c15e38655dae84aaea50682c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d67f26fe81ff34cc712f8e206540a39",
+    "url": "/mern-boilerplate/front-end/index.html"
+  },
+  {
+    "revision": "9d8ea43eb26f7024dd6e",
+    "url": "/mern-boilerplate/front-end/static/css/main.b526b87f.chunk.css"
+  },
+  {
+    "revision": "f91f2bb9bbef2f57c2b2",
+    "url": "/mern-boilerplate/front-end/static/js/2.29925a8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mern-boilerplate/front-end/static/js/2.29925a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d8ea43eb26f7024dd6e",
+    "url": "/mern-boilerplate/front-end/static/js/main.f31ee827.chunk.js"
+  },
+  {
+    "revision": "3258597877dba63d886b",
+    "url": "/mern-boilerplate/front-end/static/js/runtime-main.8267b8d2.js"
+  }
+]);
