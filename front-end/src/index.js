2d21cca07a5734be51f20d5403a551925139f663
u-zo <kwoojo0127@naver.com>
2020-09-18 18:45:07 +0900

    Update index.js to hide values of states

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer, { rootSaga } from './modules';
 import { tempSetUser, check } from './modules/user';
@@ -14,8 +14,8 @@ import { tempSetUser, check } from './modules/user';
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(sagaMiddleware)),
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
 
 function loadUser() {
@@ -34,12 +34,12 @@ sagaMiddleware.run(rootSaga);
 loadUser();
 
 ReactDOM.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>,
-    document.getElementById('root'),
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
